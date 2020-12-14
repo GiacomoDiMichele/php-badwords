7f0dae61ac5da743b15cb8152c553cc0c50d7fa0
Giacomo <giacomo.1936@gmail.com>
2020-12-14 17:32:54 +0100

    applicata badword alla parola selezionata con get

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    $badword = $_GET['badword'];
+
+    $censored_word = 'test censura parola php!';
+
+    $asterisk_word = str_replace($badword, '***', $censored_word);
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -5,8 +13,16 @@
         <title></title>
     </head>
     <body>
-        <?php
-            echo "Hello World!";
-         ?>
+        <h1>
+            la parola da censurare è
+            <?php
+                echo $badword
+            ?>
+        </h1>
+        <h1>
+            <?php
+                echo $asterisk_word
+            ?>
+        </h1>
     </body>
 </html>
